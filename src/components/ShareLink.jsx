87605d4d802a65fded2fb14d114541a2d50ca86e
AreYouLucky <johnmichaelcagadas@gmail.com>
2025-07-09 17:36:59 +0800

    update sharer links

--- src/components/ShareLink.jsx
+++ src/components/ShareLink.jsx
@@ -4,10 +4,9 @@ import { FaSquareFacebook } from "react-icons/fa6";
 
 const ShareLink = ({ article }) => {
   const location = useLocation();
-  const currentUrl = window.location.origin + location.pathname;
+  const currentUrl = window.location.href;
 
-  const shareUrl = `https://www.facebook.com/sharer/sharer.php?u=${currentUrl}`;
-  console.log(currentUrl);
+  const shareUrl = `https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(currentUrl)}`;
 
   return (
     <>
@@ -15,22 +14,23 @@ const ShareLink = ({ article }) => {
         <title>{article.title}</title>
         <meta property="og:title" content={article.title} />
         <meta property="og:description" content={article.excerpt} />
-        <meta property="og:image" content={`${window.location.origin}/images/articles/ ${article.title}`} />
-        <meta property="og:url" content={window.location.href} />
+        <meta
+          property="og:image"
+          content={`${window.location.origin}/images/articles/${article.image}`}
+        />
+        <meta property="og:url" content={currentUrl} />
       </Helmet>
       <a
         href={shareUrl}
         target="_blank"
         rel="noopener noreferrer"
-        className=""
       >
         <span className='flex mt-8 bg-blue-600 w-fit px-3 py-2 rounded-md text-white montserrat-bold text-sm items-center'>
-          <FaSquareFacebook className='text-2xl mr-2'></FaSquareFacebook>
+          <FaSquareFacebook className='text-2xl mr-2' />
           Share on Facebook
         </span>
       </a>
     </>
-
   );
 };
 
