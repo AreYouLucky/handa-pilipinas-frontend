1c0721a9b0938f8bb9087b66ada32841b51a5a00
AreYouLucky <johnmichaelcagadas@gmail.com>
2025-07-09 17:20:16 +0800

    update sharelinks

--- src/components/ShareLink.jsx
+++ src/components/ShareLink.jsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import { Helmet } from 'react-helmet';
 import { useLocation } from 'react-router-dom';
+import { FaSquareFacebook } from "react-icons/fa6";
 
-const ShareLink = () => {
+const ShareLink = ({ article }) => {
   const location = useLocation();
   const currentUrl = window.location.origin + location.pathname;
 
@@ -9,14 +10,27 @@ const ShareLink = () => {
   console.log(currentUrl);
 
   return (
-    <a
-      href={shareUrl}
-      target="_blank"
-      rel="noopener noreferrer"
-      style={{ color: 'blue', textDecoration: 'underline' }}
-    >
-      Share on Facebook
-    </a>
+    <>
+      <Helmet>
+        <title>{article.title}</title>
+        <meta property="og:title" content={article.title} />
+        <meta property="og:description" content={article.excerpt} />
+        <meta property="og:image" content={`${window.location.origin}/images/articles/ ${article.title}`} />
+        <meta property="og:url" content={window.location.href} />
+      </Helmet>
+      <a
+        href={shareUrl}
+        target="_blank"
+        rel="noopener noreferrer"
+        className=""
+      >
+        <span className='flex mt-8 bg-blue-600 w-fit px-3 py-2 rounded-md text-white montserrat-bold text-sm items-center'>
+          <FaSquareFacebook className='text-2xl mr-2'></FaSquareFacebook>
+          Share on Facebook
+        </span>
+      </a>
+    </>
+
   );
 };
 
