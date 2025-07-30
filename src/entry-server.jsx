70f54e2aed6ffe225ebd7d16d4e4b4d5002a1cd0
AreYouLucky <johnmichaelcagadas@gmail.com>
2025-07-30 17:51:27 +0800

    react ssr

---
+++ src/entry-server.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { StaticRouterProvider } from "react-router-dom/server";
+import { createStaticHandler, createStaticRouter } from "react-router-dom/server";
+import { routes } from "./routes/routes";
+
+export async function render(url, res) {
+  const { query, dataRoutes } = createStaticHandler(routes);
+  const context = await query(url);
+
+  const router = createStaticRouter(dataRoutes, context);
+
+  const jsx = <StaticRouterProvider router={router} context={context} />;
+
+  return jsx;
+}
